1e3e73a0a531ec2b2556f01bf5d38af02606a064
Gustav <sir.gustav.the.coder@gmail.com>
2018-09-02 20:51:06 +0200

    changing worlds, and the 2 start worlds

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -4,6 +4,8 @@ extends Area2D
 # var a = 2
 # var b = "textvar"
 
+export(String, FILE, "*.tscn") var next_world
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
@@ -19,5 +21,6 @@ func _on_Door_body_entered(body):
 	if body.name == "Player":
 		if body.has_mail:
 			print("lets go")
+			get_tree().change_scene(next_world)
 		else:
 			print("Where is my mail?")
