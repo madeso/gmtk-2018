59f306ddb8d996cb895b7185d94f5a71cdd616ab
Gustav <sir.gustav.the.coder@gmail.com>
2018-09-02 19:43:20 +0200

    global sounds that arent stopped when level restart

--- global-sounds.gd
+++ global-sounds.gd
@@ -1,8 +1,11 @@
-extends Area2D
+extends Node2D
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+
+func play_spikes():
+	$OnSpikes.play()
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -13,11 +16,3 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-
-
-func _on_Spikes_body_entered(body):
-	if body.name == "Player":
-		global_sounds.play_spikes()
-		# $Sfx.play()
-		print("restart level")
-		get_tree().reload_current_scene()
